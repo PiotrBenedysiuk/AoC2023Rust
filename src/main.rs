3e6eb046604b95a6971c750720ee953b875dd043
Piotr Benedysiuk <benedysiuk.piotr@gmail.com>
2024-01-24 20:23:26 +0100

    Struggling to implement the freaking tokenizer

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod day_1;
+
+fn main() {
+   day_1::main::solve(false);
+   day_1::main::solve(true);
+   day_1::buffer_tokenizer::better_solve(false);
+   day_1::buffer_tokenizer::better_solve(true);
+}
